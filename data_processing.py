6ccc09dbc13e1df979ef2e255c54763e3e81dfe4
aniketdeshmukh8486 <aniket.deshmukh8486@gmail.com>
2022-09-30 19:01:51 +0530

    addded 2 ML model code

---
+++ data_processing.py
@@ -0,0 +1,17 @@
+print("Adding all the data processing code related frad detection project")
+
+a = 5
+b = 20
+
+def sum(a,b):
+    c = a+b
+    return c
+
+print("Sum Funtion")
+
+def multiplication(a,b):
+    c = a*b
+    return c
+
+print("Multiplication Funtion")
+
---
+++ ml_model.py
@@ -0,0 +1,6 @@
+print("Hey New File..")
+
+
+def module 1():
+    print("Adding code for ML module")
+
